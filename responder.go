92098c38071b11b6541a6c7e202b0962e1362d5a
Nathan Clark <Nathan.Clark@tokenshift.com>
2014-10-14 19:56:48 -0400

    Adding PUT and DELETE.

--- responder.go
+++ responder.go
@@ -58,8 +58,10 @@ func (r Responder) Handle(res http.ResponseWriter, req *http.Request) {
 		r.handleGet(res, path)
 	case "PUT":
 		// Create or update a file.
+		r.handlePut(res, req, path)
 	case "DELETE":
 		// Delete a file.
+		r.handleDelete(res, path)
 	default:
 		LogInfo(r, "Unsupported request method:", req.Method)
 		res.WriteHeader(400)
@@ -94,4 +96,28 @@ func (r Responder) handleGet(res http.ResponseWriter, path string) {
 		LogInfo(r, "Content-Length:", fs.Size())
 		res.Header()["Content-Length"] = []string{fmt.Sprint(fs.Size())}
 		io.Copy(res, f)
+}
+
+func (r Responder) handleDelete(res http.ResponseWriter, path string) {
+	err := os.Remove(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			LogInfo(r, "File not found:", path)
+			res.WriteHeader(404)
+		} else {
+			LogError(r, "Failed to delete file:", path, err)
+			res.WriteHeader(500)
+		}
+	}
+}
+
+func (r Responder) handlePut(res http.ResponseWriter, req *http.Request, path string) {
+	f, err := os.Create(path)
+	if err != nil {
+		LogError(r, err)
+		res.WriteHeader(500)
+		return
+	}
+
+	io.Copy(f, req.Body)
 }
